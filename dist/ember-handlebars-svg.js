79358ab76f9234d7689efaf97e92527188bcee3c
Ben Lesh <blesh@netflix.com>
2014-06-18 11:31:58 -0700

    updated the build

--- dist/ember-handlebars-svg.js
+++ dist/ember-handlebars-svg.js
@@ -8,9 +8,16 @@
 		throw new Error('Unable to patch Ember Handlebars, _metamorphWrapMap not found');
 	}
 
-	var tags = 'altglyph altglyphdef altglyphitem animate animatecolor animatemotion animatetransform circle clippath color-profile cursor defs desc ellipse feblend fecolormatrix fecomponenttransfer fecomposite feconvolvematrix fediffuselighting fedisplacementmap fedistantlight feflood fefunca fefuncb fefuncg fefuncr fegaussianblur feimage femerge femergenode femorphology feoffset fepointlight fespecularlighting fespotlight fetile feturbulence filter font font-face font-face-format font-face-name font-face-src font-face-uri foreignobject g glyph glyphref hkern image line lineargradient marker mask metadata missing-glyph mpath path pattern polygon polyline radialgradient rect set stop switch symbol text textpath title tref tspan use view vkern'.split(' ');
+	Ember._metamorphSvgTags = ['altGlyph', 'altGlyphDef', 'altGlyphItem', 'animate', 'animateColor', 'animateMotion', 'animateTransform', 'circle', 
+	'clipPath', 'color-profile', 'cursor', 'defs', 'desc', 'ellipse', 'feBlend', 'feColorMatrix', 'feComponentTransfer', 'feComposite', 
+	'feConvolveMatrix', 'feDiffuseLighting', 'feDisplacementMap', 'feDistantLight', 'feFlood', 'feFuncA', 'feFuncB', 'feFuncG', 
+	'feFuncR', 'feGaussianBlur', 'feImage', 'feMerge', 'feMergeNode', 'feMorphology', 'feOffset', 'fePointLight', 'feSpecularLighting', 
+	'feSpotLight', 'feTile', 'feTurbulence', 'filter', 'font', 'font-face', 'font-face-format', 'font-face-name', 'font-face-src', 
+	'font-face-uri', 'foreignObject', 'g', 'glyph', 'glyphRef', 'hkern', 'image', 'line', 'linearGradient', 'marker', 'mask', 'metadata', 
+	'missing-glyph', 'mpath', 'path', 'pattern', 'polygon', 'polyline', 'radialGradient', 'rect', 'set', 'stop', 'svg', 'switch', 
+	'symbol', 'text', 'textPath', 'title', 'tref', 'tspan', 'use', 'view', 'vkern'];
 
-	tags.forEach(function(tag) {
+	Ember._metamorphSvgTags.forEach(function(tag){
 		Ember._metamorphWrapMap[tag] = [1, '<svg>', '</svg>'];
 	});
 }(Ember, jQuery));
